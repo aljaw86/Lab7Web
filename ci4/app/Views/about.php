23bf0577e37935f52f463fd65166a49fc3844021
aljaw86 <wahyualdi.r86@gmail.com>
2025-06-12 22:01:18 +0800

    Add files via upload

---
+++ ci4/app/Views/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/main') ?>
+
+<?= $this->section('content') ?>
+
+<h1><?= $title; ?></h1>
+
+<div class="content">
+    <h2>Tentang Kami</h2>
+    <p>Ini adalah halaman about dari website artikel kami.</p>
+   
+</div>
+
+<?= $this->endSection() ?>
